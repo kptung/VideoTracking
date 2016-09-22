9f8039a0377d9b40460a4e42827cf86f3fb6027d
kassia <kassia@iii.org.tw>
2016-09-22 17:29:18 +0800

    [Refactoring] Modify Javadoc

--- videotracking/src/main/java/org/iii/snsi/videotracking/NativeCamshift.java
+++ videotracking/src/main/java/org/iii/snsi/videotracking/NativeCamshift.java
@@ -12,97 +12,105 @@ public class NativeCamshift {
 	}
 	private long handle;
 
+	/**
+	 * Constructor
+	 */
 	public NativeCamshift() {
 		handle = initialize();
 	}
 
-	public int initCamshift(byte[] imageNV21, int width, int height,
-			int[] initRect) {
-		return initCamshiftA(handle, imageNV21, width, height, initRect);
+	/**
+	 * The function to initialize Camshift algorithm
+	 *
+	 * @param image The NV21 image.
+	 * @param width The image width.
+	 * @param height The image height.
+	 * @param rect The four elements one dimension array that indecate a
+	 * retangle. The data format is [x, y, w, h]
+	 * @return A postive rectangle id, return -1 if error occurred.
+	 */
+	public int initrectangle(byte[] image, int width, int height,
+			int[] rect) {
+		return initCamshiftA(handle, image, width, height, rect);
 	}
 
-	public int initCamshift(byte[] imageNV21, int width, int height,
-			Rect rectangle) {
-		return initCamshiftO(handle, imageNV21, width, height, rectangle);
+	/**
+	 * The function to initialize Camshift algorithm
+	 *
+	 * @param image The NV21 image.
+	 * @param width The image width.
+	 * @param height The image height.
+	 * @param rect The retangle to be track.
+	 * @return A postive rectangle id, return -1 if error occurred.
+	 */
+	public int initCamshift(byte[] image, int width, int height,
+			Rect rect) {
+		return initCamshiftO(handle, image, width, height, rect);
 	}
 
-	public int processCamshift(byte[] imageNV21, int width, int height,
-			int[] rectList) {
-		return processCamshiftA(handle, imageNV21, width, height, rectList);
+	/**
+	 * Processing Camshit to track rectangles
+	 *
+	 * @param image The NV21 image.
+	 * @param width The image width.
+	 * @param height The image height.
+	 * @param rects The one dimension integer array to save results. <br />
+	 * i.e. [0,122,20,45,78,1,23,23,100,20,2,-1,-1,-1,-1] means the following:
+	 * RectangleID 0: x = 122, y = 20, w = 45, h = 78.<br />
+	 * RectangleID 1: x = 23, y = 23, w = 100, h = 20.<br />
+	 * RectangleID 2: False alarm.
+	 * @return Return -1 if error occured, otherwise return 0.
+	 */
+	public int processCamshift(byte[] image, int width, int height,
+			int[] rects) {
+		return processCamshiftA(handle, image, width, height, rects);
 	}
 
-	public int processCamshift(byte[] imageNV21, int width, int height,
-			HashMap<Integer, Rect> rectList) {
-		return processCamshiftO(handle, imageNV21, width, height, rectList);
+	/**
+	 * Processing Camshit to track rectangles
+	 *
+	 * @param image The NV21 image.
+	 * @param width The image width.
+	 * @param height The image height.
+	 * @param rects The hash map to save results.
+	 * @return Return -1 if error occured, otherwise return 0.
+	 */
+	public int processCamshift(byte[] image, int width, int height,
+			HashMap<Integer, Rect> rects) {
+		return processCamshiftO(handle, image, width, height, rects);
 	}
 
-	public void deleteRectangle(int rectangleID) {
-		deleteRectangle(handle, rectangleID);
+	/**
+	 * The function to release rectangle object
+	 *
+	 * @param id The rectangle id that is returned by initCamshift.
+	 */
+	public void deleteRectangle(int id) {
+		deleteRectangle(handle, id);
 	}
 
+	/**
+	 * The function to release Camshift algorithm
+	 */
 	public void releaseHandle() {
 		releaseHandle(handle);
 	}
 
-	/**
-	 * @return A structure pointer.
-	 */
 	private native long initialize();
 
-	/**
-	 * @param handle Structure pointer.
-	 * @param image Input NV21image.
-	 * @param width Image width.
-	 * @param height Image height.
-	 * @param rect A size = 4 int array that contains top left position of the
-	 * rectangle and size. i.e. [x, y, w, h]
-	 * @return Rectangle ID, ID should >=0, return -1 if initialize failed.
-	 */
 	private native int initCamshiftA(long handle, byte[] image, int width,
 			int height, int[] rect);
 
-	/**
-	 * @param handle Structure pointer.
-	 * @param image Input NV21image.
-	 * @param width Image width.
-	 * @param height Image height.
-	 * @param rect Rectangle.
-	 * @return Rectangle ID, ID should >=0, return -1 if initialize failed.
-	 */
 	private native int initCamshiftO(long handle, byte[] image, int width,
 			int height, Rect rect);
 
-	/**
-	 * @param handle Structure pointer.
-	 * @param image Input NV21image.
-	 * @param width Image width.
-	 * @param height Image height.
-	 * @param rects i.e. [0,122,20,45,78,1,23,23,100,20,2,-1,-1,-1,-1] means the
-	 * following: RectangleID 0: x = 122, y = 20, w = 45, h = 78. RectangleID 1:
-	 * x = 23, y = 23, w = 100, h = 20. RectangleID 2: False alarm.
-	 * @return Return -1 if error occured, 0 otherwise.
-	 */
 	private native int processCamshiftA(long handle, byte[] image, int width,
 			int height, int[] rects);
 
-	/**
-	 * @param handle Structure pointer.
-	 * @param image Input NV21image.
-	 * @param width Image width.
-	 * @param height Image height.
-	 * @param rects Result list in hash map.
-	 * @return Return -1 if error occured, 0 otherwise.
-	 */
 	private native int processCamshiftO(long handle, byte[] image, int width,
 			int height, HashMap<Integer, Rect> rects);
 
-	/**
-	 * @param id The id is returned from initCamshift.
-	 */
 	private native void deleteRectangle(long handle, int id);
 
-	/**
-	 * @param handle Structure pointer.
-	 */
 	private native void releaseHandle(long handle);
 }
