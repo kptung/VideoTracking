ca62d00b1657fe6594cde610fc3e6313b2d25c31
jimchen <jimchen@iii.org.tw>
2018-10-11 10:19:25 +0800

    [Modify] Update camera library.

--- app/src/main/java/org/iii/snsi/trackingtest/MainActivity.java
+++ app/src/main/java/org/iii/snsi/trackingtest/MainActivity.java
@@ -14,37 +14,37 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
-
-import org.iii.snsi.multimedia.Camera2;
-import org.iii.snsi.multimedia.OldCamera;
-import org.iii.snsi.videotracking.NativeTracking;
-
 import java.io.File;
 import java.util.ArrayList;
-
+import org.iii.snsi.multimedia.Camera2;
+import org.iii.snsi.multimedia.OldCamera;
+import org.iii.snsi.streamlibrary.CameraController;
+import org.iii.snsi.videotracking.NativeTracking;
 import static android.content.ContentValues.TAG;
 
-public class MainActivity extends Activity{
+public class MainActivity extends Activity {
 
+	/// multi-objs tracking initialization
+	ArrayList<Integer> rect = new ArrayList<Integer>();
+	ArrayList<Integer> other = new ArrayList<Integer>();
+	//
+	int x = 0;
 	// Button initialization
 	private Button cameraButton;
 	private Button saveButton;
 	private Button trackButton;
-
 	/// display classes;  sufaceView shows the camera captured screen preview;
-	/// mCamera is to control camera; mView is to draw the rect when camera is opened
+	/// mCamera is to control camera; mView is to draw the rect when camera is
+	/// opened
 	private SurfaceView surfaceView;
 	private OldCamera mCamera;
 	private Camera2 mCamera2;
 	private TouchView mView;
-
 	///  show the available drawing rect on touchview
 	private Rect rec = new Rect();
-
 	// Thread initialization
 	private Thread drawThread;
 	private boolean threadFlag;
-
 	// Tracking initialization
 	private NativeTracking tracker;
 	private int mScreenHeight;
@@ -57,39 +57,45 @@ public class MainActivity extends Activity{
 	private float wRatio;
 	private float hRatio;
 	/// a number of tracking objs
-	private int objcount=0;
-	private int trackcount=0;
-
-	/// multi-objs tracking initialization
-	ArrayList<Integer> rect = new ArrayList<Integer>();
-	ArrayList<Integer> other = new ArrayList<Integer>();
-
+	private int objcount = 0;
+	private int trackcount = 0;
+	private int format = NativeTracking.FORMAT_NV21;
 	/// the flag to open camera
-	private boolean cameraflag=false;
+	private boolean cameraflag = false;
 	/// the flag to process real-time video tracking
 	private boolean camtrackflag = false;
 	/// the flag to save real-time rectangle
 	private boolean camsaveflag = false;
 	/// the flag to process multi-objs tracking
 	private boolean track_multiobj_flag = true;
-
-	//
-	int x=0;
+	private Handler mHandler = new Handler() {
+		@Override
+		public void handleMessage(Message msg) {
+			switch (msg.what) {
+				case 1:
+					mView.setVisibility(View.INVISIBLE);
+					mView.setVisibility(View.VISIBLE);
+					break;
+			}
+		}
+	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		//Log.i(TAG, "onCreate()");
-		tracker= new NativeTracking();
+		tracker = new NativeTracking();
 
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
 		// display our (only) XML layout - Views already ordered
 		setContentView(R.layout.activity_main);
 
-		// according to device screen size 2 get the window width and height to display buttons
+		// according to device screen size 2 get the window width and height
+		// to display buttons
 		DisplayMetrics displaymetrics = new DisplayMetrics();
 		getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
 		mScreenHeight = displaymetrics.heightPixels;
@@ -106,14 +112,14 @@ public class MainActivity extends Activity{
 			previewHeight = mCamera2.getHeight();
 		}
 
-		wRatio=(float)previewWidth/mScreenWidth;
-		hRatio=(float)previewHeight/mScreenHeight;
+		wRatio = (float) previewWidth / mScreenWidth;
+		hRatio = (float) previewHeight / mScreenHeight;
 		/// draw camera view @ this surfaceview
-		surfaceView = (SurfaceView)findViewById(R.id.surfaceView);
+		surfaceView = (SurfaceView) findViewById(R.id.surfaceView);
 		/// button initialization
-		cameraButton = (Button)findViewById(R.id.button1);
-		saveButton = (Button)findViewById(R.id.button2);
-		trackButton = (Button)findViewById(R.id.button3);
+		cameraButton = (Button) findViewById(R.id.button1);
+		saveButton = (Button) findViewById(R.id.button2);
+		trackButton = (Button) findViewById(R.id.button3);
 
 		cameraButton.setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -126,10 +132,17 @@ public class MainActivity extends Activity{
 					mCamera.setCallbackFrameListener(
 							new OldCamera.CallbackFrameListener() {
 								@Override
+								public void onEncodingTexturePrepared() {
+
+								}
+
+								@Override
 								public void onCallbackFrame(byte[] data,
-										int width, int height) {
+										int width, int height, int format) {
+									formatRecognition(format);
 									preview = data;
 								}
+
 								@Override
 								public void onCallbackPicture(File file) {
 
@@ -140,8 +153,14 @@ public class MainActivity extends Activity{
 					mCamera2.setCallbackFrameListener(
 							new Camera2.CallbackFrameListener() {
 								@Override
+								public void onEncodingTexturePrepared() {
+
+								}
+
+								@Override
 								public void onCallbackFrame(byte[] data,
-										int width, int height) {
+										int width, int height, int format) {
+									formatRecognition(format);
 									preview = data;
 								}
 
@@ -150,9 +169,12 @@ public class MainActivity extends Activity{
 
 								}
 							});
+					mCamera2.setFrameFormat(
+							CameraController.CallbackFrameListener
+									.FRAME_FORMAT_Y);
 					mCamera2.setSurfaceHolder(holder);
 					mCamera2.openCamera();
-					mCamera2.startPreview(true, true, false);
+					mCamera2.startPreview(true, false, false);
 				}
 
 				tracker = new NativeTracking();
@@ -163,8 +185,8 @@ public class MainActivity extends Activity{
 			@Override
 			public void onClick(View v) {
 				Log.i(TAG, "save roi position");
-				objcount+=1;
-				camsaveflag=true;
+				objcount += 1;
+				camsaveflag = true;
 			}
 		});
 
@@ -172,7 +194,7 @@ public class MainActivity extends Activity{
 			@Override
 			public void onClick(View v) {
 				Log.i(TAG, "strat tracking");
-				camtrackflag=true;
+				camtrackflag = true;
 			}
 		});
 
@@ -180,64 +202,66 @@ public class MainActivity extends Activity{
 
 		mView = (TouchView) findViewById(R.id.left_top_view);
 		/// calcualte the rect moving distance
-		rec.set((int) ((double) mScreenWidth),
-				(int) ((double) mScreenHeight),
-				(int) ((double) mScreenWidth),
-				(int) ((double) mScreenHeight));
+		rec.set((int) ((double) mScreenWidth), (int) ((double) mScreenHeight),
+				(int) ((double) mScreenWidth), (int) ((double) mScreenHeight));
 		/// set the rect position for boundary checking
 		mView.setRec(rec);
 
-		drawThread = new Thread(new Runnable(){
+		drawThread = new Thread(new Runnable() {
 			@Override
-			public void run(){
-				while(threadFlag){
-					if(camsaveflag) {
-						// rect down-sampling : ( OpenCV Rect (left-top x, left-top y, width, height))
+			public void run() {
+				while (threadFlag) {
+					if (camsaveflag) {
+						// rect down-sampling : ( OpenCV Rect (left-top x,
+						// left-top y, width, height))
 						int lx = Math.round(mView.getmLeftTopPosX() * wRatio);
 						int ly = Math.round(mView.getmLeftTopPosY() * hRatio);
-						int width=Math.round((mView.getmRightTopPosX() - mView.getmLeftTopPosX()) * wRatio);
-						int height=Math.round((mView.getmRightBottomPosY() - mView.getmRightTopPosY()) * hRatio);
+						int width = Math.round((mView.getmRightTopPosX() -
+								mView
+								.getmLeftTopPosX()) * wRatio);
+						int height = Math.round(
+								(mView.getmRightBottomPosY() - mView
+										.getmRightTopPosY()) * hRatio);
 
 						// Specify a tracking target
-						if(objcount==1)
-						{
+						if (objcount == 1) {
 							rect.add(lx);
 							rect.add(ly);
 							rect.add(width);
 							rect.add(height);
 							int[] roi = convert2intArray(rect);
 							// init tracking
-							int[] initarr=tracker.addTrackingObjects(preview, previewWidth, previewHeight, roi);
-						}
-						else if(objcount>1)
-						{
+							int[] initarr = tracker.addTrackingObjects(preview,
+									previewWidth, previewHeight, roi);
+						} else if (objcount > 1) {
 							other.add(lx);
 							other.add(ly);
 							other.add(width);
 							other.add(height);
 						}
-						camsaveflag=false;
+						camsaveflag = false;
 					}
-					if(camtrackflag){
-						trackcount+=1;
-						if(objcount>1 && trackcount==1)
-						{
+					if (camtrackflag) {
+						trackcount += 1;
+						if (objcount > 1 && trackcount == 1) {
 							//all.addAll(other);
 							int[] others = convert2intArray(other);
-							int[] addarr = tracker.addTrackingObjects(preview, previewWidth, previewHeight, others);
+							int[] addarr = tracker.addTrackingObjects(preview,
+									previewWidth, previewHeight, others);
 						}
 						/// tracking
-						int[] rects = (int []) tracker.processTracking(preview, NativeTracking.FORMAT_NV21);
+						int[] rects = (int[]) tracker.processTracking(preview,
+								format);
 						// rect up-sampling
-						for(int i=0;i<rects.length;i+=5) {
-							int lx=rects[i + 1];
-							int ly=rects[i + 2];
-							int w=rects[i + 3];
-							int h=rects[i + 4];
+						for (int i = 0; i < rects.length; i += 5) {
+							int lx = rects[i + 1];
+							int ly = rects[i + 2];
+							int w = rects[i + 3];
+							int h = rects[i + 4];
 							rects[i + 1] = Math.round(lx / wRatio);
 							rects[i + 2] = Math.round(ly / hRatio);
-							rects[i + 3] = Math.round((lx + w) / wRatio );
-							rects[i + 4] = Math.round((ly + h) / hRatio );
+							rects[i + 3] = Math.round((lx + w) / wRatio);
+							rects[i + 4] = Math.round((ly + h) / hRatio);
 						}
 
 						/// update Rects
@@ -257,18 +281,6 @@ public class MainActivity extends Activity{
 
 	}
 
-	private Handler mHandler = new Handler(){
-		@Override
-		public void handleMessage(Message msg) {
-			switch(msg.what){
-				case 1:
-					mView.setVisibility(View.INVISIBLE);
-					mView.setVisibility(View.VISIBLE);
-					break;
-			}
-		}
-	};
-
 	@Override
 	protected void onDestroy() {
 
@@ -282,7 +294,7 @@ public class MainActivity extends Activity{
 		threadFlag = false;
 	}
 
-	public void sleep(long time){
+	public void sleep(long time) {
 		try {
 			Thread.sleep(time);
 		} catch (InterruptedException e) {
@@ -293,10 +305,27 @@ public class MainActivity extends Activity{
 	public int[] convert2intArray(ArrayList<Integer> IntegerList) {
 		int[] intArray = new int[IntegerList.size()];
 		int count = 0;
-		for(int i : IntegerList){
+		for (int i : IntegerList) {
 			intArray[count++] = i;
 		}
 		return intArray;
+	}
+
+	private void formatRecognition(int frameFormat) {
+		switch (frameFormat) {
+			//FRAME_FORMAT_NV21
+			case 0:
+				format = NativeTracking.FORMAT_NV21;
+				break;
+			//FRAME_FORMAT_Y
+			case 1:
+				format = NativeTracking.FORMAT_GRAY;
+				break;
+			//FRAME_FORMAT_YUV420P
+			case 2:
+				format = NativeTracking.FORMAT_YUV420P;
+				break;
+		}
 	}
 
 }
